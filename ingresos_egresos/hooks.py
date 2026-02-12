448e6be38966b514e84d732ab17098e3fce21a13
Ernesto Ruiz <eruiz@AsoFinde.local>
2026-02-12 14:13:50 -0600

    feat: update Egreso fixtures with Salario, Comisiones and alphabetical order

--- ingresos_egresos/hooks.py
+++ ingresos_egresos/hooks.py
@@ -10,6 +10,13 @@ app_license = "mit"
 
 required_apps = ["erpnext"]
 
+fixtures = [
+    {
+        "dt": "IE Codigo",
+        "filters": [["name", "in", ["Ingreso", "Egreso"]]]
+    }
+]
+
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
 # 	{
